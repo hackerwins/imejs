1ab96a49e8059c7314a32f9494f787cb44727a0b
Youngteac Hong <susukang98@gmail.com>
2012-12-09 16:04:16 +0900

    support hangul 390, under construction

--- ime.js
+++ ime.js
@@ -14,7 +14,7 @@
      * @param elHolder for eventListener (optional, default: body element)
      */
     this.init = function(fnHandler, elHolder) {
-      this.aIm = [imH2, imEN, imH3];
+      this.aIm = [imH2, imEN, imH390];
       this.currentIdx = 0;
       this.setEventListener(elHolder || document.body, fnHandler);
     };
@@ -525,13 +525,383 @@
   })();
 
   /***************************
-   * hangul 3
+   * hangul 390 (by blueiur)
+   * under construction
    ***************************/
-  var imH3 = new (function() {
-    this.name = 'H3';
-    this.handleKeyEvent = function(e) {
-      return {};
+  var imH390 = new (function() {
+    this.name = 'H390';
+    this.buf = new buf();
+
+    //handleKeyEvent for hangul 390
+    this.handleKeyEvent = function(oKeyEvent) {
+			if (key.isAlpha(oKeyEvent.ch) && key.SPACE != oKeyEvent.ch){
+				var sCmd = this.buf.size() == 0 ? 'insChar' : 'cmbChar';
+				var aHangul = this.parse(this.buf.get() + String.fromCharCode(oKeyEvent.ch));
+				return {name:sCmd, value:aHangul.join("")};
+			}else{
+				if (oKeyEvent.ch == key.ENTER){
+					this.buf.flush();
+					return {name:'insPara'};
+				}else if (oKeyEvent.ch == key.BACKSPACE){
+					if (this.buf.size() > 1){
+						this.buf.init(1);
+						return {name:"cmbChar", value:this.parse(this.buf.get())};
+					}else{
+						this.buf.flush();
+						return {name:'delChar'};
+					}
+				}else if (key.isAlpha(oKeyEvent.ch)){
+					this.buf.flush();
+					return {name:'insChar', value:String.fromCharCode(oKeyEvent.ch)};
+				}else{
+					this.buf.flush();
+					return {};
+				}
+			}
+		};
+
+		this.parse = function(stream){
+      this.buf.flush();
+			var cho = -1, vowel = -1, jong = -1;
+			var flushed = [];
+
+      var aCh = stream.split('');
+      for (var idx = 0, len = aCh.length; idx < len; idx++) {
+        var ch = aCh[idx];
+			  this.buf.push(ch);
+				var newCho = this.getIndex("cho", ch);
+				var newVowel = this.getIndex("vowel", ch);
+        var newJong = this.getIndex("jong", ch);
+
+				//0. initial state
+				if (cho < 0 && vowel < 0 && jong < 0){
+					if (newCho >= 0){ //01. ㄱ
+						cho = newCho;
+					}else if (newVowel >= 0){ //02. ㅏ
+						vowel = newVowel;
+					}else if (newJong >= 0){
+            cho = this.getChoFromJong(newJong);
+          }
+				}else if (cho >= 0 && vowel < 0 && jong < 0){
+					if (newCho >= 0){ //11. ㄱ+ㄱ, check flush
+						var combineCho = this.getCombineCho(cho, newCho);
+						if (combineCho >= 0){
+							cho = combineCho;
+            }else{
+							this.buf.tail(1);
+							flushed.push(this.getHangulFromCho(cho));
+							cho = newCho;
+						}
+					}else if (newVowel >= 0){ //12. ㄱ+ㅏ
+						vowel = newVowel;
+					}else if(newJong >= 0){
+						this.buf.tail(1);
+						flushed.push(this.getHangulFromCho(cho));
+            cho = this.getChoFromJong(newJong);
+          }
+				//2. 가
+				}else if (cho >= 0 && vowel >= 0 && jong < 0){					
+					if ( newCho >= 0 ){
+            this.buf.tail(1);
+            flushed.push(this.getHangul(cho, vowel, jong));
+            cho = newCho;
+            vowel = -1;
+					}else if ( newVowel >= 0 ){ //22. 가+ㅣ, check flush
+						var combineVowel = this.getCombineVowel(vowel, newVowel);
+						if (combineVowel >= 0){
+							vowel = combineVowel;
+						}else{
+							this.buf.tail(1);
+							flushed.push(this.getHangul(cho, vowel, jong));
+							cho = -1;
+							vowel = newVowel;
+						}
+					}else if ( newJong > -1 ){ //21. 가+ㅇ, check flush
+						jong = newJong;
+          }
+				//3. 강
+				}else if (cho >= 0 && vowel >= 0 && jong >= 0){
+					if (newCho >= 0){ //31. 뷀+ㄱ, check flush
+						var combineJong = this.getCombineJong(jong, newJong);
+						if (combineJong >= 0){
+							jong = combineJong;
+						}else{
+							this.buf.tail(1);
+							flushed.push(this.getHangul(cho, vowel, jong));
+							cho = newCho;
+							vowel = -1;
+							jong = -1;
+						}
+					}else if (newVowel >= 0){ //32. 강+ㅡ, flush
+            this.buf.tail(1);
+						flushed.push(this.getHangul(cho, vowel, jong));
+						cho = -1;
+						vowel = newVowel;
+						jong = -1;
+					}else if (newJong > -1){
+						var combineJong = this.getCombineJong(jong, newJong);
+						if (combineJong >= 0){
+							jong = combineJong;
+						}else{
+							this.buf.tail(1);
+							flushed.push(this.getHangul(cho, vowel, jong));
+							cho = this.getChoFromJong(newJong);
+							vowel = -1;
+							jong = -1;
+						}
+					}
+				//4. ㅏ
+				}else if (cho < 0 && vowel >= 0 && jong < 0){
+					if (newCho >= 0){ //41. ㅏ + ㄱ = 가, *)adjust typo
+						cho = newCho;
+          }else if (newVowel >= 0){ //42. ㅗ + ㅐ = ㅙ
+						var combineVowel = this.getCombineVowel(vowel, newVowel);
+						if (combineVowel >= 0){
+							vowel = newVowel;
+						}else{
+							this.buf.tail(1);
+							flushed.push(this.getHangul(cho, vowel, jong));
+							cho = -1;
+							vowel = newVowel;
+						}
+					}else if (newJong >= 0){
+            cho = this.getChoFromJong(newJong);
+          }
+				}
+			};
+      
+			if ( cho > -1 || vowel > -1 || jong > -1){ // jong first
+				flushed.push(this.getHangul(cho, vowel, jong));
+			}
+
+			return flushed;
+		};
+
+		/**
+		 * combine chosung (ex: ㄱ+ㄱ=ㄲ)
+		 **/
+		this.getCombineCho = function( cho1, cho2 ){
+			var ch = this.aCho[cho1];
+			if (cho1 == cho2){
+				return this.getIndex("cho", ch + ch); //ㄱ -> ㄲ
+			}else{
+				return -1;
+			}
+		};
+
+		/**
+		 * combine vowel (ex: ㅜ+ㅓ=ㅝ)
+		 **/
+		this.getCombineVowel = function( vowel1, vowel2 ){
+			return this.getIndex("vowel", this.aVowel[vowel1] + this.aVowel[vowel2]);
+		};
+
+		/**
+		 * combine jong (ex: ㄴ+ㅈ=ㄴㅈ)
+		 **/
+		this.getCombineJong = function( jong, newJong ){
+			return this.getIndex("jong", this.aJong[jong] + this.aJong[newJong]);
+		};
+
+		/**
+		 * 0 -> ㄱ
+		 **/
+		this.getHangulFromCho = function( cho ){
+			var idx = this.aJaso.indexOf(this.aCho[cho]);
+			return String.fromCharCode(12593 + idx);
+		};
+
+		/**
+		 * 0 -> ㅏ
+		 **/
+		this.getHangulFromVowel = function( vowel ){
+			var idx = this.aJaso.indexOf(this.aVowel[vowel]);
+			return String.fromCharCode(12593 + idx);
+		};
+
+		/**
+		 * 0,0,0 -> 가
+		 **/
+		this.getHangul = function( cho, vowel, jong ){
+			if (cho >= 0 && vowel >= 0){
+				jong = jong == -1 ? 0 : jong;
+				return String.fromCharCode((cho * 21 + vowel) * 28 + jong + 44032);
+			}else if (cho >= 0){
+				return this.getHangulFromCho(cho);
+			}else if (vowel >= 0){
+				return this.getHangulFromVowel(vowel);
+			}else{
+				throw "IllegalArgument";
+			}
+		};
+
+    this.getIndex = function(name, ch){
+      if (name == "cho"){
+        return this.aCho.indexOf(ch);
+      }else if (name == "vowel"){
+        if (ch == "/"){ ch = "v";};
+        if (ch == "9"){ ch = "b";};
+        return this.aVowel.indexOf(ch);
+      }else if (name == "jong"){
+        return this.aJong.indexOf(ch);
+      }
+
+      return -1;
     };
+
+		this.getChoFromJong = function(jong){
+			var map ={
+				"x" : "k", //ㄱ
+				"s" : "h", //ㄴ
+				"A" : "u",//ㄷ
+				"w" : "y", //ㄹ
+				"z" : "i",//ㅁ
+				"3" : ";",//ㅂ
+				"q" : "n",//ㅅ
+				"a" : "j",//ㅇ
+				"!" : "i",//ㅈ
+				"Z" : "o",//ㅊ
+				"e" : "0",//ㅋ
+				"W" : "'",//ㅌ
+				"Q" : "p",//ㅍ
+				"1" : "m"//ㅎ
+			};
+			var ch = map[ this.aJong[jong] ];
+      return this.aCho.indexOf( ch );
+		};
+
+		this.aCho = [
+			"k",//ㄱ
+			"kk",//ㄲ
+			"h",//ㄴ
+			"u",//ㄷ
+			"uu",//ㄸ
+			"y",//ㄹ
+			"i",//ㅁ
+			";",//ㅂ
+			";;",//ㅃ
+			"n",//ㅅ
+			"nn",//ㅆ
+			"j",//ㅇ
+			"l",//ㅈ
+			"ll",//ㅉ
+			"o",//ㅊ
+			"0",//ㅋ
+			"'",//ㅌ
+			"p",//ㅍ
+			"m" //ㅎ
+		];
+
+		this.aVowel = [
+	    "f",//ㅏ
+	    "r",//ㅐ
+	    "6",//ㅑ
+	    "R",//ㅒ
+	    "t",//ㅓ
+	    "c",//ㅔ
+	    "e",//ㅕ
+	    "7",//ㅖ
+	    "v",//ㅗ
+	    "vf",//ㅘ
+	    "vr",//ㅙ
+	    "vd",//ㅚ
+	    "4",//ㅛ
+	    "b",//ㅜ
+	    "bt",//ㅝ
+	    "bc",//ㅞ
+	    "bd",//ㅟ
+	    "5",//ㅠ
+	    "g",//ㅡ
+	    "gd",//ㅢ
+	    "d" //ㅣ
+		];
+
+		this.aJong = [
+			"", //padding
+			"x",//ㄱ
+	    "F",//ㄲ
+	    "xq",//ㄳ
+	    "s",//ㄴ
+	    "s!",//ㄵ
+	    "S",//ㄶ
+	    "A",//ㄷ
+	    "w",//ㄹ
+	    "wx",//ㄺ
+	    "wz",//ㄻ
+	    "w3",//ㄼ
+	    "wq",//ㄽ
+	    "wW",//ㄾ
+	    "wQ",//ㄿ
+	    "V",//ㅀ
+	    "z",//ㅁ
+	    "3",//ㅂ
+	    "3q",//ㅄ
+	    "q",//ㅅ
+	    "2",//ㅆ
+	    "a",//ㅇ
+	    "!",//ㅈ
+	    "Z",//ㅊ
+	    "e",//ㅋ
+	    "W",//ㅌ
+	    "Q",//ㅍ
+	    "1" //ㅎ
+		];
+
+		//String.fromCharCode(12593 + idx)
+		this.aJaso = [
+			"k",//ㄱ
+			"kk",//ㄲ
+			"xq",//ㄳ
+			"h",//ㄴ
+			"s!",//ㄵ
+			"S",//ㄶ
+			"u",//ㄷ
+			"uu",//ㄸ
+			"y",//ㄹ
+			"wx",//ㄺ
+			"wz",//ㄻ
+			"w3",//ㄼ
+			"wq",//ㄽ
+			"wW",//ㄾ
+			"wQ",//ㄿ
+			"V",//ㅀ
+			"i",//ㅁ
+			";",//ㅂ
+			";;",//ㅃ
+			"3q",//ㅄ
+			"n",//ㅅ
+			"nn",//ㅆ
+			"j",//ㅇ
+			"l",//ㅈ
+			"ll",//ㅉ
+			"o",//ㅊ
+			"0",//ㅋ
+			"'",//ㅌ
+			"p",//ㅍ
+			"m",//ㅎ
+			"f",//ㅏ
+			"r",//ㅐ
+			"6",//ㅑ
+			"R",//ㅒ
+			"t",//ㅓ
+			"c",//ㅔ
+			"e",//ㅕ
+			"7",//ㅖ
+			"v",//ㅗ
+			"vf",//ㅘ
+			"vr",//ㅙ
+			"vd",//ㅚ
+			"4",//ㅛ
+			"b",//ㅜ
+			"bt",//ㅝ
+			"bc",//ㅞ
+			"bd",//ㅟ
+			"5",//ㅠ
+			"g",//ㅡ
+			"gdl",//ㅢ
+			"d" //ㅣ
+		];
+
   })();
 
   root.ime = new ime();
